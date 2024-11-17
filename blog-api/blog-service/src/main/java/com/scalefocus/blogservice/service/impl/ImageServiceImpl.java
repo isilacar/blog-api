a490fc8e847c0aac58f1a032c559777dd0d67bb5
Işıl Acar <isilacar@hotmail.com.tr>
2024-11-17 16:44:27 +0300

    fixing tests

--- blog-api/blog-service/src/main/java/com/scalefocus/blogservice/service/impl/ImageServiceImpl.java
+++ blog-api/blog-service/src/main/java/com/scalefocus/blogservice/service/impl/ImageServiceImpl.java
@@ -13,8 +13,8 @@ import com.scalefocus.blogservice.service.ImageService;
 import com.scalefocus.blogservice.utils.BlogUtil;
 import com.scalefocus.blogservice.utils.UserClientUtil;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
@@ -35,8 +35,7 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class ImageServiceImpl implements ImageService {
 
-    private static final Logger log = LoggerFactory.getLogger(ImageServiceImpl.class);
-
+    private static final Logger log = LogManager.getLogger(ImageServiceImpl.class);
     @Value(("${file.path}"))
     private String uploadDir;
 
